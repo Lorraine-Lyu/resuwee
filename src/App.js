3af74fedbb51c83e6e8bcbadbabca4549fd91981
Lorraine-Lyu <llyu@smith.edu>
2019-07-03 15:10:43 +0800

    separated style into independent folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
 import styled from 'styled-components';
 import {contact, user, education} from './util.js';
 
